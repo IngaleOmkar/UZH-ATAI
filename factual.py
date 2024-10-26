b67500486a2af1c0752c9f5a72bfb69c222d7ee0
Omkar Ingale <51068625+IngaleOmkar@users.noreply.github.com>
2024-10-26 16:07:21 +0200

    usuing both user intent classifiers

--- factual.py
+++ factual.py
@@ -3,38 +3,63 @@ import re
 from intent_classifier import EmbeddingBasedIntentClassifier
 
 class FactualResponder(Responder):
-    def __init__(self, data_repository, entity_extractor, intent_classifier):
-        super().__init__(data_repository, entity_extractor, intent_classifier)
+    def __init__(self, data_repository, entity_extractor, mlp_intent_classifier, emb_intent_classifier):
+        super().__init__(data_repository, entity_extractor, mlp_intent_classifier)
         self.label_to_uri = self.data_repository.get_label_to_uri()
         self.triplets = self.data_repository.get_triplets()
         self.uri_to_label = self.data_repository.get_uri_to_label()
 
-        self.split = False
-
-        if(type(self.intent_classifier) == EmbeddingBasedIntentClassifier):
-            self.split = True
+        self.emb_intent_classifier = emb_intent_classifier
+        self.mlp_intent_classifier = mlp_intent_classifier
 
 
     def answer_query(self, query):
         entities = self.entity_extractor.get_guaranteed_entities(query)
 
-        if(not self.split):
-            # Classify the query
-            tag, uri = self.intent_classifier.classify_query(query)
-        else:
-            tag, uri = self.intent_classifier.classify_query(" ".join(query.split(entities[0])))
-            print(tag, uri)
+
+        tag_mlp, uri_mlp = self.mlp_intent_classifier.classify_query(query)
+        tag_emb, uri_emb = self.emb_intent_classifier.classify_query(" ".join(query.split(entities[0])))
+
 
         if(len(entities) == 0):
             raise Exception("I'm sorry, I couldn't understand the query.")
         else:
             en_uri = self.label_to_uri[entities[0]]
-            try:
-                if(tag in ["rating", "revenue", "budget", "release_date"] ):
-                   return self.triplets[(en_uri, uri)]
-                else:
-                    ans = self.triplets[(en_uri, uri)]
-                    ans_labels = [self.uri_to_label[label] for label in ans]
-                    return ", ".join(ans_labels)
-            except:
-                raise Exception("I'm sorry, I couldn't find the answer to your question.")
+
+            if(tag_mlp == tag_emb):
+                # both models agree
+                try:
+                    if(tag_mlp in ["rating", "revenue", "budget", "release_date"] ):
+                        return self.triplets[(en_uri, uri_mlp)]
+                    else:
+                        ans = self.triplets[(en_uri, uri_mlp)]
+                        ans_labels = [self.uri_to_label[label] for label in ans]
+                        return ", ".join(ans_labels)
+                except:
+                    pass
+            
+            
+            if tag_mlp != tag_emb:
+                # models disagree,  try embedding model first
+                try:
+                    if(tag_emb in ["rating", "revenue", "budget", "release_date"] ):
+                        return self.triplets[(en_uri, uri_emb)]
+                    else:
+                        ans = self.triplets[(en_uri, uri_emb)]
+                        ans_labels = [self.uri_to_label[label] for label in ans]
+                        return ", ".join(ans_labels)
+                except:
+                    pass
+
+                # if embedding model fails, try mlp model
+                try:
+                    if(tag_mlp in ["rating", "revenue", "budget", "release_date"] ):
+                        return self.triplets[(en_uri, uri_mlp)]
+                    else:
+                        ans = self.triplets[(en_uri, uri_mlp)]
+                        ans_labels = [self.uri_to_label[label] for label in ans]
+                        return ", ".join(ans_labels)
+                except:
+                    pass
+
+            raise Exception("I'm sorry, I couldn't find the answer to your question.")
