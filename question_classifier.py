9fc8fabb61d95b1148a697c918284a262b30bc8a
Omkar Ingale <51068625+IngaleOmkar@users.noreply.github.com>
2024-11-13 22:44:45 +0100

    working recommender system

---
+++ question_classifier.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+
+class QuestionClassifier():
+
+    def __init__(self):
+        self.model = None
+        self.model = BertForSequenceClassification.from_pretrained("data/q_classifier_qnr")
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.id2tag = {0: 'qna', 1: 'recommendation'}
+    
+    def classify(self, query)->str:
+        inputs = self.tokenizer(query, return_tensors="pt")
+        outputs = self.model(**inputs)
+        predicted_label = torch.argmax(outputs.logits[0]).item()
+        return self.id2tag[predicted_label]
