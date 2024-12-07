25c30a99a2201d58937e2ab9f69080e54b0fd07f
Omkar Ingale <51068625+IngaleOmkar@users.noreply.github.com>
2024-12-07 02:44:24 +0100

    local testing file and minor changes

--- local_bot.py
+++ local_bot.py
@@ -1,4 +1,5 @@
-from speakeasypy import Speakeasy, Chatroom
+# FOR LOCAL TESTING ONLY. 
+
 from typing import List
 import time
 from embeddings import EmbeddingsResponder
@@ -13,12 +14,8 @@ from question_classifier import QuestionClassifier
 from image import ImageResponder
 from crowd import CrowdsourceResoponder
 
-
-DEFAULT_HOST_URL = 'https://speakeasy.ifi.uzh.ch'
-listen_freq = 2
-
 class Agent:
-    def __init__(self, username, password):
+    def __init__(self):
 
         self.data_repository = DataRepository()
         
@@ -32,58 +29,6 @@ class Agent:
         self.image = ImageResponder(self.data_repository, self.extractor, self.emb_intent_classifier)
         self.question_classifier = QuestionClassifier()
         self.crowd = CrowdsourceResoponder(self.data_repository, self.extractor, self.emb_intent_classifier)
-
-        self.username = username
-        # Initialize the Speakeasy Python framework and login.
-        self.speakeasy = Speakeasy(host=DEFAULT_HOST_URL, username=username, password=password)
-        self.speakeasy.login()  # This framework will help you log out automatically when the program terminates.
-
-    def listen(self):
-        while True:
-            # only check active chatrooms (i.e., remaining_time > 0) if active=True.
-            rooms: List[Chatroom] = self.speakeasy.get_rooms(active=True)
-            for room in rooms:
-                if not room.initiated:
-                    # send a welcome message if room is not initiated
-                    room.post_messages(f'Hello! This is a welcome message from {room.my_alias}.')
-                    room.initiated = True
-                # Retrieve messages from this chat room.
-                # If only_partner=True, it filters out messages sent by the current bot.
-                # If only_new=True, it filters out messages that have already been marked as processed.
-                for message in room.get_messages(only_partner=True, only_new=True):
-                    print(
-                        f"\t- Chatroom {room.room_id} "
-                        f"- new message #{message.ordinal}: '{message.message}' "
-                        f"- {self.get_time()}")
-
-                    # Send a message to the corresponding chat room using the post_messages method of the room object.
-                    try:
-                        answer = self.answer(message.message)
-                        answer = answer.encode('latin-1', errors='replace').decode('latin-1')
-                        print(answer)
-                        room.post_messages(answer)
-
-                    except Exception as e:
-                        room.post_messages(f"An error occurred: {e}")
-                        print(e)
-
-                    # Mark the message as processed, so it will be filtered out when retrieving new messages.
-                    room.mark_as_processed(message)
-
-                # Retrieve reactions from this chat room.
-                # If only_new=True, it filters out reactions that have already been marked as processed.
-                for reaction in room.get_reactions(only_new=True):
-                    print(
-                        f"\t- Chatroom {room.room_id} "
-                        f"- new reaction #{reaction.message_ordinal}: '{reaction.type}' "
-                        f"- {self.get_time()}")
-
-                    # Implement your agent here #
-
-                    room.post_messages(f"Received your reaction: '{reaction.type}' ")
-                    room.mark_as_processed(reaction)
-
-            time.sleep(listen_freq)
 
     def answer(self, query):
         question_type = self.question_classifier.classify(query)
@@ -177,7 +122,7 @@ class Agent:
         try:
             results = self.image.answer_query(query)
             imgs = ["image:" + x for x in results]
-            return imgs.join(" \n")
+            return imgs
         except Exception as e:
             return "I am very sorry, but no answer was found."
         
@@ -194,5 +139,8 @@ class Agent:
 
 
 if __name__ == '__main__':
-    demo_bot = Agent("sharp-cloud", "Q1jeQ8I2")
-    demo_bot.listen()
+    demo_bot = Agent()
+    while True:
+        query = input("Ask me a question: ")
+        print(demo_bot.answer(query))
+        print("\n")
